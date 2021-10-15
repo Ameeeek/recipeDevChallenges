27c4d6595f2bd1a9c5167754cd4d0dfe0d86ca4a
amek <ameeeekWork@gmail.com>
2021-10-15 13:29:34 +0800

    menambahkan config tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        besar: "48px",
+        sedang: "24px",
+        kecil: "18px",
+        subtitle: "16px",
+        chips: "14px",
+        besarHP: "24px",
+        sedangHP: "18px",
+        kecilHP: "16px",
+        subtitleHP: "14px",
+        chipsHP: "12px"
+      },
+      screens:{
+        'hp':{'max': '858px'},
+      }
+    },
   },
   variants: {
     extend: {},
